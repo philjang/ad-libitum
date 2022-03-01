29ac688b0fb3b924632ce72230c6eda945308bce
philjang <pjang7390@gmail.com>
2022-02-28 20:24:00 -0800

    add routes to categories.js for get / post / and get /:name, modify corresponding ejs files

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,21 +1,52 @@
-require('dotenv').config()
-const express = require('express')
-const router = express.Router()
-const db = require('../models')
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
 
-router.get('/', (req,res)=>{
-    res.render('categories/index.ejs')
-})
+router.get('/', async (req, res) => {
+    try {
+        const categories = await db.category.findAll({
+            where: { userId: res.locals.currentUser.id },
+        });
+        res.render('categories/index.ejs', { categoryArr: categories });
+    } catch (error) {
+        console.log(error);
+    }
+});
 
-router.get('/new', (req,res)=>{
-    res.render('categories/new.ejs')
-})
+router.post('/', async (req, res) => {
+    try {
+        const [newCategory, wasCreated] = await db.category.findOrCreate({
+            where: {
+                name: req.body.name,
+                userId: res.locals.currentUser.id,
+            },
+        });
+        console.log(`A new category, ${newCategory.name}, was created: ${wasCreated}`);
+        res.redirect('/categories');
+    } catch (error) {
+        console.log(error);
+    }
+});
 
-router.get('/:name', (req,res)=>{
-    res.render('categories/show.ejs')
-})
+router.get('/new', (req, res) => {
+    res.render('categories/new.ejs');
+});
 
+router.get('/:name', async (req, res) => {
+    try {
+        const selectedCategory = await db.category.findOne({
+            where: {
+                name: req.params.name,
+                userId: res.locals.currentUser.id,
+            },
+            include: [db.restaurant]
+        });
+        const restaurants = selectedCategory.restaurants
+        res.render('categories/show.ejs', {restaurantArr: restaurants,selectedCategory});
+    } catch (error) {
+        console.log(error);
+    }
+});
 
-
-
-module.exports = router
+module.exports = router;
