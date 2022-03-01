88d3cdba2a8f338e843150e660f653b1bf790b61
philjang <pjang7390@gmail.com>
2022-03-01 12:02:58 -0800

    add user auth to every page other than home,signup, and login

--- controllers/categories.js
+++ controllers/categories.js
@@ -4,49 +4,57 @@ const router = express.Router();
 const db = require('../models');
 
 router.get('/', async (req, res) => {
-    try {
-        const categories = await db.category.findAll({
-            where: { userId: res.locals.currentUser.id },
-        });
-        res.render('categories/index.ejs', { categoryArr: categories });
-    } catch (error) {
-        console.log(error);
-    }
+    if (res.locals.currentUser) {
+        try {
+            const categories = await db.category.findAll({
+                where: { userId: res.locals.currentUser.id },
+            });
+            res.render('categories/index.ejs', { categoryArr: categories });
+        } catch (error) {
+            console.log(error);
+        }
+    } else res.redirect('/')
 });
 
 router.post('/', async (req, res) => {
-    try {
-        const [newCategory, wasCreated] = await db.category.findOrCreate({
-            where: {
-                name: req.body.name,
-                userId: res.locals.currentUser.id,
-            },
-        });
-        console.log(`User ${res.locals.currentUser.name} created a new category, ${newCategory.name}: ${wasCreated}`);
-        res.redirect('/categories');
-    } catch (error) {
-        console.log(error);
-    }
+    if (res.locals.currentUser) {
+        try {
+            const [newCategory, wasCreated] = await db.category.findOrCreate({
+                where: {
+                    name: req.body.name,
+                    userId: res.locals.currentUser.id,
+                },
+            });
+            console.log(`User ${res.locals.currentUser.name} created a new category, ${newCategory.name}: ${wasCreated}`);
+            res.redirect('/categories');
+        } catch (error) {
+            console.log(error);
+        }
+    } else res.redirect('/')
 });
-
+    
 router.get('/new', (req, res) => {
-    res.render('categories/new.ejs');
+    if (res.locals.currentUser) {
+        res.render('categories/new.ejs');
+    } else res.redirect('/')
 });
 
 router.get('/:name', async (req, res) => {
-    try {
-        const selectedCategory = await db.category.findOne({
-            where: {
-                name: req.params.name,
-                userId: res.locals.currentUser.id,
-            },
-            include: [db.restaurant]
-        });
-        const restaurants = selectedCategory.restaurants
-        res.render('categories/show.ejs', {restaurantArr: restaurants,selectedCategory});
-    } catch (error) {
-        console.log(error);
-    }
+    if (res.locals.currentUser) {
+        try {
+            const selectedCategory = await db.category.findOne({
+                where: {
+                    name: req.params.name,
+                    userId: res.locals.currentUser.id,
+                },
+                include: [db.restaurant]
+            });
+            const restaurants = selectedCategory.restaurants
+            res.render('categories/show.ejs', {restaurantArr: restaurants,selectedCategory});
+        } catch (error) {
+            console.log(error);
+        }
+    } else res.redirect('/')
 });
 
 module.exports = router;
--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -6,87 +6,98 @@ const axios = require('axios')
 
 // restaurant list page
 router.get('/', async (req,res)=>{
-    try {
-        const restaurants = await db.restaurant.findAll({
-            where: {userId: res.locals.currentUser.id}
-        })
-        res.render('restaurants/index.ejs', {restaurantArr: restaurants})
-    } catch (error) {
-        console.log(error)
-    }
+    if (res.locals.currentUser) {
+        try {
+            const restaurants = await db.restaurant.findAll({
+                where: {userId: res.locals.currentUser.id}
+            })
+            res.render('restaurants/index.ejs', {restaurantArr: restaurants})
+        } catch (error) {
+            console.log(error)
+        }
+    } else res.redirect('/')
 })
 
 // add new restaurant
 router.post('/', async (req,res) => {
-    try {
-        const [newRestaurant, wasCreated] = await db.restaurant.findOrCreate({
-            where: {
-                name: req.body.name,
-                userId: res.locals.currentUser.id,
-            },
-            defaults: {
-                address: req.body.address,
-                note: req.body.note,
-                rating: req.body.rating
-            }
-        })
-        console.log(`User ${res.locals.currentUser.name} created a new restaurant, ${newRestaurant.name}: ${wasCreated}`)
-        const retrievedJSON = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/"+newRestaurant.name+" "+newRestaurant.address+".json?types=address%2Cpoi&access_token="+process.env.MAPBOX_API_TOKEN)
-        // console.log(retrievedJSON.data.features[0].center)
-        const point = await {type: 'Point', coordinates: retrievedJSON.data.features[0].center}
-        newRestaurant.coordinates = point
-        await newRestaurant.save()
-        // console.log(newRestaurant.coordinates)
-        res.redirect('/restaurants')
-    } catch (error) {
-        console.log(error)
-    }
+    if (res.locals.currentUser) {
+        try {
+            const [newRestaurant, wasCreated] = await db.restaurant.findOrCreate({
+                where: {
+                    name: req.body.name,
+                    userId: res.locals.currentUser.id,
+                },
+                defaults: {
+                    address: req.body.address,
+                    note: req.body.note,
+                    rating: req.body.rating
+                }
+            })
+            console.log(`User ${res.locals.currentUser.name} created a new restaurant, ${newRestaurant.name}: ${wasCreated}`)
+            const retrievedJSON = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/"+newRestaurant.name+" "+newRestaurant.address+".json?types=address%2Cpoi&access_token="+process.env.MAPBOX_API_TOKEN)
+            // console.log(retrievedJSON.data.features[0].center)
+            const point = await {type: 'Point', coordinates: retrievedJSON.data.features[0].center}
+            newRestaurant.coordinates = point
+            await newRestaurant.save()
+            // console.log(newRestaurant.coordinates)
+            res.redirect('/restaurants')
+        } catch (error) {
+            console.log(error)
+        }
+    } else res.redirect('/')
 })
 
 // new restaurant form
 router.get('/new', (req,res)=>{
-    res.render('restaurants/new.ejs')
+    if (res.locals.currentUser) {
+        res.render('restaurants/new.ejs')
+    } else res.redirect('/')
 })
 
 // edit restaurant form
 router.get('/edit/:id', async (req,res)=>{
-    try {
-
-        res.render('restaurants/edit.ejs')
-    } catch (error) {
-        console.log(error)
-    }
+    if (res.locals.currentUser) {
+        try {
+            
+            res.render('restaurants/edit.ejs')
+        } catch (error) {
+            console.log(error)
+        }
+    } else res.redirect('/')
 })
 
 // add new menu item
 router.post('/:id/', (req,res) => {
-    console.log('posted new menu to restaurant with id:'+req.params.id)
-    res.redirect(`/restaurants/${req.params.id}`)
+    if (res.locals.currentUser) {
+        console.log('posted new menu to restaurant with id:'+req.params.id)
+        res.redirect(`/restaurants/${req.params.id}`)
+    } else res.redirect('/')
 })
 
 // new menu item form
 router.get('/:id/new', (req,res) => {
-    res.render('restaurants/newmenu.ejs', {restaurantId: req.params.id})
+    if (res.locals.currentUser) {
+        res.render('restaurants/newmenu.ejs', {restaurantId: req.params.id})
+    } else res.redirect('/')
 })
 
 // restaurant details page
 router.get('/:id', async (req,res)=>{
-    try {
-        const selectedRestaurant = await db.restaurant.findOne({
-            where: {
-                id: req.params.id,
-                userId: res.locals.currentUser.id,
-            },
-            include: [db.menu]
-        })
-        const menuItems = selectedRestaurant.menus
-        res.render('restaurants/show.ejs', {menuArr: menuItems, selectedRestaurant})
-    } catch (error) {
-        console.log(error)
-    }
+    if (res.locals.currentUser) {
+        try {
+            const selectedRestaurant = await db.restaurant.findOne({
+                where: {
+                    id: req.params.id,
+                    userId: res.locals.currentUser.id,
+                },
+                include: [db.menu]
+            })
+            const menuItems = selectedRestaurant.menus
+            res.render('restaurants/show.ejs', {menuArr: menuItems, selectedRestaurant})
+        } catch (error) {
+            console.log(error)
+        }
+    } else res.redirect('/')
 })
-
-
-
 
 module.exports = router
